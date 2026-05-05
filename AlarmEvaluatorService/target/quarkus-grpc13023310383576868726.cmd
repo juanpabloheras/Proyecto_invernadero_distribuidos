0b75aea800eb55c3356ef242ed839d20873913a3
juan-pablitoINSOF <juan.heras247054@potros.itson.edu.mx>
2026-05-05 00:26:17 -0700

    Removí artefactos de build y target

--- AlarmEvaluatorService/target/quarkus-grpc13023310383576868726.cmd
+++
@@ -1,2 +0,0 @@
-@echo off
-"C:\Program Files\Eclipse Adoptium\jdk-21.0.10.7-hotspot\bin\java.EXE" -cp "C:\Users\chris\.m2\repository\io\quarkus\quarkus-grpc-protoc-plugin\3.15.1\quarkus-grpc-protoc-plugin-3.15.1-shaded.jar" io.quarkus.grpc.protoc.plugin.MutinyGrpcGenerator
