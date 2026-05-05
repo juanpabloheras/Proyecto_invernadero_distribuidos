96c4c86019c32f4602eae2b39a8a9412f5481142
Christopher Alvarez Centeno <christopher.alvarez251954@potros.itson.edu.mx>
2026-05-04 19:22:56 -0700

    Ya se pueden registrar alarmas
    ya se pueden registrar diferentes tipos de alarmas sin necesidad de volver a levantar el servicio

--- AlarmEvaluatorService/src/main/java/itson/mx/alarmevaluatorservice/services/AlarmaNotificacionSGrpcServiceImpl.java
+++ AlarmEvaluatorService/src/main/java/itson/mx/alarmevaluatorservice/services/AlarmaNotificacionSGrpcServiceImpl.java
@@ -6,11 +6,14 @@ package itson.mx.alarmevaluatorservice.services;
 
 import io.quarkus.grpc.GrpcService;
 import io.smallrye.mutiny.Uni;
+import itson.mx.alarmevaluatorservice.cache.ConfiguracionCache;
 import itson.mx.alarmevaluatorservice.grpc.AlarmaNotificacionService;
+import itson.mx.alarmevaluatorservice.grpc.ConfiguracionAlarma;
 import itson.mx.alarmevaluatorservice.grpc.ConfiguracionAlarmaCreadaRequest;
 import itson.mx.alarmevaluatorservice.grpc.ConfiguracionesResponse;
 import itson.mx.alarmevaluatorservice.grpc.NotificacionResponse;
 import itson.mx.alarmevaluatorservice.grpc.ObtenerConfiguracionesRequest;
+import jakarta.inject.Inject;
 
 /**
  *
@@ -18,6 +21,9 @@ import itson.mx.alarmevaluatorservice.grpc.ObtenerConfiguracionesRequest;
  */
 @GrpcService
 public class AlarmaNotificacionSGrpcServiceImpl implements AlarmaNotificacionService {
+
+    @Inject
+    ConfiguracionCache configuracionCache;
 
     @Override
     public Uni<NotificacionResponse> notificarConfiguracionCreada(ConfiguracionAlarmaCreadaRequest request) {
@@ -31,6 +37,18 @@ public class AlarmaNotificacionSGrpcServiceImpl implements AlarmaNotificacionSer
         System.out.println("Valor crítico: " + request.getValorCritico());
         System.out.println("Activa: " + request.getActiva());
         System.out.println("====================================");
+
+        if (request.getActiva()) {
+            ConfiguracionAlarma config = ConfiguracionAlarma.newBuilder()
+                    .setIdConfiguracionAlarma(request.getIdConfiguracionAlarma())
+                    .setNombreAlarma(request.getNombreAlarma())
+                    .setTipoAlarma(request.getTipoAlarma())
+                    .setOperador(request.getOperador())
+                    .setValorCritico(request.getValorCritico())
+                    .setActiva(request.getActiva())
+                    .build();
+            configuracionCache.agregarOActualizar(config);
+        }
 
         return Uni.createFrom().item(
                 NotificacionResponse.newBuilder()
